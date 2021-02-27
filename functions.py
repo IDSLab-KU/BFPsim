568a882be18558a279da768f8369b51a88ea8646
R3C0D3r <cryptographcode@gmail.com>
2021-02-28 04:23:03 +0900

    feat(func): make_groups_tensor added
    This gradually increased speed of executing make_groups.
    It'll removed shortly after

--- functions.py
+++ functions.py
@@ -124,3 +124,43 @@ def set_mantissa_tensor(inp, mb):
     # revert to original np.float32 
     r = np.frombuffer(r_, dtype=np.float32)
     return torch.from_numpy(r.reshape(inp_n.shape))
+
+# make_group_tensor : Group values as same exponent bits, which shifts mantissa
+# TODO : Set direction of grouping
+def make_groups_tensor(inp, mb, group_size = 36, direction = None):
+    inp_n = inp.numpy() # inp_n = inp # For debug,
+    # Convert to byte stream
+    st = inp_n.tobytes() 
+    # Set to uint32 array to easy computing
+    v = np.frombuffer(st, dtype=np.uint32) 
+    # Extract exponent
+    e_mask = np.full(v.shape, 0x7f800000, dtype=np.uint32)
+    e_ = np.bitwise_and(v, e_mask)
+    # Get the max value
+    # IDEA : send shift code to back, maybe that's faster
+    np.right_shift(e_, 23, out=e_)
+    # Match shape to divisible to group size
+    m_ = np.append(e_, np.zeros(group_size - e_.shape[0] % group_size, dtype=np.uint32))
+    m_ = np.reshape(m_, (group_size, -1))
+    # get the max value of each blocks
+    m_ = np.amax(m_, axis=0)
+    # Revert back to original size
+    m_ = np.repeat(m_, group_size)
+    # Match shape back to input
+    m_ = m_[:e_.shape[0]]
+
+    # Difference of the exponent
+    # -1 is for on grouping, IEEE's basic mantissa bit has to be included to the value, so...
+    e_ = mb - 1 - (m_ - e_)
+    # Clip the negative value (I know this is not smarter way)
+    e_[e_ > 0xff] = 0
+    # np.clip(e_, 0, 0xff, out=e_) # Options...
+    r_mask = np.full(v.shape, 0x007fffff, dtype=np.uint32)
+    # Shift to make reversed mask
+    np.right_shift(r_mask, e_, out=r_mask)
+    # Get the reversed mask
+    np.invert(r_mask, out=r_mask)
+    r_ = np.bitwise_and(v, r_mask)
+    # revert to original np.float32 
+    r = np.frombuffer(r_, dtype=np.float32)
+    return torch.from_numpy(r.reshape(inp_n.shape))
