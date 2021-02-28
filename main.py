43308f67ca35054590624be000caab0521680f10
R3C0D3r <cryptographcode@gmail.com>
2021-02-28 19:31:13 +0900

    fix : rebasing codes

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
-from blocks import BFLinear
+from net import SimpleNet
 
 from log import Log
+
 import signal
 import sys
+import argparse
 
+def str2bool(v):
+    if v.lower() in ["true", "t", "1"]: return True
+    elif v.lower() in ["false", "f", "0"]: return False
+    else: raise argparse.ArgumentTypeError("Not Boolean value")
 
 exitToken = 10
 def ExitCounter():
@@ -20,40 +26,42 @@ def ExitCounter():
     if exitToken == 0:
         sys.exit()
 
-
-
 def handler(signum, frame):
     # Log.Exit()
     print('Quit by user', signum)
     sys.exit()
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=0)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=0)
+def ArgumentParse(print=True):
+    s = "List of the training arguments\n"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--dataset", type=str, default = "CIFAR-10",
+        help = "Dataset to use [CIFAR-10, CIFAR-100]")
+    parser.add_argument("-m","--model", type=str, default = "SimpleNet",
+        help = "Model to use [SimpleNet, Resnet18]")
 
-        self.fc1 = BFLinear(64 * 5 * 5, 1024)
-        self.fc2 = BFLinear(1024, 1024)
-        self.fc3 = BFLinear(1024, 10)
+    # Training setup
+    parser.add_argument("-bst","--batch-size-training", type=int, default = 4,
+        help = "Size of the mini-batch on training")
+    parser.add_argument("-bse","--batch-size-evaluation", type=int, default = 4,
+        help = "Size of the mini-batch on evaluation")
+    parser.add_argument("-e","--training-epochs", type=int, default = 5,
+        help = "Number of epochs to train")
 
-    def forward(self, x):
-                                        #  3x32x32
-        x = F.relu(self.conv1(x))       # 16x32x32
-        x = self.pool1(x)               # 16x16x16
-        x = F.relu(self.conv2(x))       # 32x14x14
-        x = self.pool2(x)               # 32x 7x 7
-        x = F.relu(self.conv3(x))       # 64x 5x 5
-        x = x.view(-1, 64 * 5 * 5)      # 1600
-        x = F.relu(self.fc1(x))         # 1024
-        x = F.relu(self.fc2(x))         # 1024
-        x = self.fc3(x)                 # 10
-        return x
-    
+
+    # Printing method
+    parser.add_argument("-pti","--print-train-interval", type=int, default = 2500,
+        help = "Print interval")
+    args = parser.parse_args()
+
+    s += args + "\n"
+
+    if print:
+        Log.Print(s, current=False, elapsed=False)
+    return args
+
+
+
 
 
 
@@ -71,13 +79,14 @@ def Evaluate(net, testloader):
 
 
 
-def Train(net, trainloader, testloader):
+def Train(net, args, trainloader, testloader):
+    # Sometimes, there need to optimize optimizer and criterion
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    lr = 0.0001
+    epoch_train = args.training_epochs
 
-    for epoch in range(5):  # loop over the dataset multiple times
+    for epoch_current in range(epoch_train):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -92,61 +101,70 @@ def Train(net, trainloader, testloader):
             loss = criterion(outputs, labels)
             loss.backward()
 
-            # Manually designed optimizer
             optimizer.step()
-            """
-            with torch.no_grad():
-                net.conv1.weight -= lr * net.conv1.weight.grad
-                net.conv2.weight -= lr * net.conv2.weight.grad
-                net.conv3.weight -= lr * net.conv3.weight.grad
-                net.fc1.weight -= lr * net.fc1.weight.grad
-                net.fc2.weight -= lr * net.fc2.weight.grad
-                net.fc3.weight -= lr * net.fc3.weight.grad
-            """
 
             # Exit after few iterations to check it's working
             # ExitCounter()
 
             # print statistics
             running_loss += loss.item()
-            if (i + 1) % 2500 == 0:    # print every 2000 mini-batches
-                Log.Print('[%d, %5d/%5d] loss: %.3f' %
-                    (epoch + 1, i + 1, len(trainloader), running_loss / 2500))
+            if args.print_train_interval != 0 and (i + 1) % args.print_train_interval == 0:    # print every 2000 mini-batches
+                Log.Print('[%d/%d, %5d/%5d] loss: %.3f' %
+                    (epoch_current + 1, epoch_train, i + 1, len(trainloader), running_loss / 2500))
                 running_loss = 0.0
                 
-        Evaluate(net, testloader)
+        Evaluate(net, args, testloader)
     Log.Print('Finished Training')
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handler)
     
+    args = ArgumentParse()
+
     # Set logger preset
     Log.SetLogFile(True)
 
-    # Set transform
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    if args.dataset == "CIFAR-10":
+        # Set transform
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # Prepare Cifar-10 Dataset
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=2)
+        # Prepare Cifar-10 Dataset
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size_training,shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,shuffle=False, num_workers=2)
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size_evaluation,shuffle=False, num_workers=2)
 
-    classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    
+        classes = ('plane', 'car', 'bird', 'cat',
+                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    elif args.dataset =="CIFAR-100":
+        transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
+        ])
+
+        raise NotImplementedError("Dataset {} not Implemented".format(args.dataset))
+    else:
+        raise NotImplementedError("Dataset {} not Implemented".format(args.dataset))
     
     # Define the network
-    net = Net()
+    if arg.model == "SimpleNet":
+        net = SimpleNet()
+    elif args.model == "ResNet18":
+        raise NotImplementedError("Model {} not Implemented".format(args.model))
+    else:
+        raise NotImplementedError("Model {} not Implemented".format(args.model))
+
+    # Print the model summary
     Log.Print("Model Summary:%s"%net,current=False, elapsed=False)
+
     # Train the network
-    Train(net, trainloader, testloader)
+    Train(net, args, trainloader, testloader)
     
-
-
 
 
 
