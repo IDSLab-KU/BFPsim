50148c9bc3450393ba4e1f649c0729c01b8f950d
R3C0D3r <cryptographcode@gmail.com>
2021-02-28 19:28:16 +0900

    feat(blocks) : applied block grouping to conv
    TODO : backward compability

--- blocks.py
+++ blocks.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-
-from bfloat import BFloat
-from bfarray import BFArray
+import torch.nn.functional as F
 
 from functions import make_groups_tensor
+
+import math
+
 
 # Temp relu, can be removed since relu doesn't need to be optimized
 class BlockReLU(torch.autograd.Function):
@@ -26,11 +27,13 @@ bit, group_size = 8, 36
 class BFLinearFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, weight, bias=None):
-        ctx.save_for_backward(input, weight, bias)
 
         # Setting NEW method!
         input = make_groups_tensor(input, bit, group_size = group_size)
         weight = make_groups_tensor(weight, bit, group_size = group_size)
+
+        # changing save position...?
+        ctx.save_for_backward(input, weight, bias)
         
         output = input.mm(weight.t())
         if bias is not None:
@@ -81,42 +84,49 @@ class BFLinear(torch.nn.Module):
 
 # Blockfloat Convolution Function
 # TODO : Implement Conv2d Operation
+# https://discuss.pytorch.org/t/implementing-a-custom-convolution-using-conv2d-input-and-conv2d-weight/18556/7
 class BFConv2dFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, weight, bias=None):
+    def forward(ctx, input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
         ctx.save_for_backward(input, weight, bias)
-        output = input.mm(weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
+
+        input = make_groups_tensor(input, bit, group_size = group_size)
+        weight = make_groups_tensor(weight, bit, group_size = group_size)
+
+        return F.conv2d(input, weight, bias, stride, padding, dilation, groups)
     
     @staticmethod
     def backward(ctx, grad_output):
-        input, weight, bias = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
+        input, weight, bias = ctx.saved_variables
+            
+        grad_input = grad_weight= grad_bias = None
 
-        grad_input = grad_output.mm(weight)
-        grad_weight = grad_output.t().mm(input) 
-        if bias is not None:
-            grad_bias = grad_output.sum(0)
-        
-        return grad_input, grad_weight, grad_bias
+        if ctx.needs_input_grad[0]:
+            grad_input = torch.nn.grad.conv2d_input(input.shape, weight, grad_output)
+            
+        if ctx.needs_input_grad[1]:
+            grad_weight = torch.nn.grad.conv2d_weight(input, weight.shape, grad_output)
+                
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0).squeeze(0)
+
+        if bias is not True:
+            return grad_input, grad_weight, grad_bias
+        else:
+            return grad_input, grad_weight
 
 
 # Blockfloat Convolution
 class BFConv2d(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, \
-        padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
+        padding=0, dilation=1, groups=1, bias=False, padding_mode='zeros'):
         super(BFConv2d, self).__init__()
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.group = group
+        self.in_channels, self.out_channels, self.kernel_size, self.stride, self.padding, self.dilation, self.groups = in_channels, out_channels, kernel_size,stride, padding, dilation, groups
         # From torch/nn/modules/conv.py
         self.weight = nn.Parameter(torch.Tensor(
-                out_channels, in_channels // groups, *kernel_size))
+                out_channels, in_channels // groups, kernel_size, kernel_size))
         if bias:
-            self.bias = Parameter(torch.Tensor(out_channels))
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter('bias', None)
         
@@ -135,16 +145,13 @@ class BFConv2d(torch.nn.Module):
 
     def extra_repr(self):
         # From /torch/nn/modules/conv.py
-        s = ('{in_channels}, {out_channels}, kernel_size={kernel_size}'
-             ', stride={stride}')
-        if self.padding != (0,) * len(self.padding):
-            s += ', padding={padding}'
-        if self.dilation != (1,) * len(self.dilation):
-            s += ', dilation={dilation}'
+        s = '{}, {}, kernel_size={}'.format(self.in_channels, self.out_channels, self.kernel_size)
+        s += ", stride={}".format(self.stride)
+        s += ', padding={}'.format(self.padding)
+        s += ', dilation={}'.format(self.dilation)
         if self.groups != 1:
-            s += ', groups={groups}'
+            s += ', groups={self.groups}'
         if self.bias is None:
             s += ', bias=False'
-        if self.padding_mode != 'zeros':
-            s += ', padding_mode={padding_mode}'
-        return s.format(**self.__dict__)
+        # return s.format(**self.__dict__)
+        return s
