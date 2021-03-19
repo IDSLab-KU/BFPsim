955505bda21ae57102729a8a1a4b8875b0a4b851
R3C0D3r <cryptographcode@gmail.com>
2021-03-19 21:11:30 +0900

    feat : applied new grouping direction convention

--- blockfunc.py
+++ blockfunc.py
@@ -36,133 +36,52 @@ def set_mantissa_tensor(inp, group_mantissa):
     r = np.frombuffer(r_, dtype=np.float32)
     return torch.from_numpy(r.reshape(inp_n.shape))
 
+# Basic size dimension, most of code will not work if this value is changed
+# This is set as constant because of expandability
+CONF_SZ = 3
+
+
 # _make_group_tensor : Group values as same exponent bits, which shifts mantissa
-# TODO : Set direction of grouping
 def make_groups_tensor(inp, group_mantissa, group_size, group_direction):
-    inp_n = inp.numpy() # inp_n = inp # For debug,
-    # Transpose to replace direction
-    inp_n = np.transpose(inp_n, group_direction) # (2,3,0,1)=kernel_input_output
-    # After transposing, change array if first two dims are not 3x3
-    # Save original dimention for later use
-    orig_shape = inp_n.shape
-    # Code modified from https://stackoverflow.com/questions/42297115/numpy-split-cube-into-cubes/42298440#42298440
-    if inp_n.shape[0] % 3 != 0:
-        # Pad Array to have more values
-        inp_n = np.pad(inp_n, ((0,3-inp_n.shape[0]%3),(0,3-inp_n.shape[1]%3),(0,0),(0,0)))
-        # Save padded size, which is used later
-        padded_shape = inp_n.shape
-        # Reshape to corresponding size to manipulate easier
-        inp_n = inp_n.reshape([inp_n.shape[0]//3, 3, inp_n.shape[1]//3, 3, inp_n.shape[2], 1, inp_n.shape[3], 1])
-        # Transpose to have 3x3 structure is preserved
-        inp_n = inp_n.transpose([4,6,0,2,1,3,5,7])
-        # Reshape array to (nx3x3x1x1)
-        inp_n = inp_n.reshape(-1, 3, 3, 1, 1)
-    # Convert to byte stream
-    st = inp_n.tobytes() 
-    # Set to uint32 array to easy computing
-    v = np.frombuffer(st, dtype=np.uint32) 
-    # Extract exponent
-    e_mask = np.full(v.shape, 0x7f800000, dtype=np.uint32)
-    e_ = np.bitwise_and(v, e_mask)
-    # Get the max value
-    # IDEA : send shift code to back, maybe that's faster
-    np.right_shift(e_, 23, out=e_)
-    # Match shape to divisible to group size
-    m_ = np.append(e_, np.zeros(group_size - e_.shape[0] % group_size, dtype=np.uint32))
-    m_ = np.reshape(m_, (group_size, -1))
-    # get the max value of each blocks
-    m_ = np.amax(m_, axis=0)
-    # Revert back to original size
-    m_ = np.repeat(m_, group_size)
-    # Match shape back to input
-    m_ = m_[:e_.shape[0]]
-    # Difference of the exponent, -1 is applied because for more accurate hardware-wise simulation
-    # On hardware, mantissa bits have to store the 1 from the IEEE Standard
-    e_ = group_mantissa - (m_ - e_) - 1
-    r_mask = np.full(v.shape, 0x007fffff, dtype=np.uint32)
-    # When mantissa have to shift more than precision bits, total value have to be zero.
-    r_mask[e_ > 0xff] = 0xffffffff
-    # Clip the negative value
-    # Maybe more smarter way...? -> np.clip(e_, 0, 0xff, out=e_)
-    e_[e_ > 0xff] = 0
-    # Shift to make reversed mask
-    np.right_shift(r_mask, e_, out=r_mask)
-    # Get the reversed mask
-    np.invert(r_mask, out=r_mask)
-    r_ = np.bitwise_and(v, r_mask)
-    # revert to original np.float32 
-    r = np.frombuffer(r_, dtype=np.float32)
-    # revert back to original shape
-    r = r.reshape(inp_n.shape)
-    # Revert back change array if first two dims are not 3x3
-    if orig_shape[0] %3 != 0:
-        # Reshape into 3x3 size
-        r = r.reshape([orig_shape[2], orig_shape[3], orig_shape[0]//3+1, orig_shape[1]//3+1, 3, 3, 1, 1])
-        # Transpose to right order
-        r = r.transpose([2,4,3,5,0,6,1,7])
-        # Revert back to transposed shape
-        r = r.reshape(padded_shape)
-        # Cut the array
-        r = r[:(orig_shape[0]%3)-3,:(orig_shape[1]%3)-3,:,:]
-    return torch.from_numpy(np.transpose(r,group_direction))
-
-BASE_SIZE = 3
-
-# _make_group_tensor : Group values as same exponent bits, which shifts mantissa
-# TODO : Set direction of grouping
-def _make_groups_tensor(inp, group_mantissa, group_size, group_direction):
     # Convert tensor to numpy array
     inp_n = inp.numpy()
+    # Save original shape
     inp_shape = inp_n.shape
-    print(inp_shape)
-    g_kernel = group_size / BASE_SIZE / BASE_SIZE
+
+    # STEP 1 : Pre-process array to match with group size
+    # Pad Array to do the grouping correctly
+    g_kernel = int(group_size / CONF_SZ / CONF_SZ)
+    if inp_n.shape[0] % g_kernel != 0:
+        inp_n = np.pad(inp_n, ((0,g_kernel-inp_n.shape[0]%g_kernel),(0,0),(0,0),(0,0)))
+    if inp_n.shape[1] % g_kernel != 0:
+        inp_n = np.pad(inp_n, ((0,0),(0,g_kernel-inp_n.shape[1]%g_kernel),(0,0),(0,0)))
+    if inp_n.shape[2] % CONF_SZ != 0 or inp_n.shape[CONF_SZ] % CONF_SZ != 0:
+        inp_n = np.pad(inp_n, ((0,0),(0,0),(0,CONF_SZ-inp_n.shape[2]%CONF_SZ),(0,CONF_SZ-inp_n.shape[3]%CONF_SZ)))
+    inp_p_shape = inp_n.shape
+
+    # transposing array to desired grouping direction
+    # TODO : Prevent overflow caused by mapping next kernel map on FX, FY, FC mode
+    # FX, FY, FC Mode have to reshape array to dimension of 8 to manipulate more easily
+    #   Code modified from https://stackoverflow.com/questions/42297115/numpy-split-cube-into-cubes/42298440#42298440
     if group_direction == 0: # WI Mode, If kernel size is not 3, it will not work properly
-        # If Kernel size is not 3, this is not working
-        # Pad the input channels to correct grouping
-        if inp_n.shape[0] % g_kernel != 0:
-            inp_n = np.pad(inp_n, ((0,g_kernel-inp_n.shape[0]%g_kernel),(0,0),(0,0),(0,0)))
-        # Transpose the array to match direction
         inp_m = np.transpose(inp_n, (2,3,0,1))
     elif group_direction == 1: # WO Mode, If kernel size is not 3, it will not work properly
-        # Pad the input channels to correct grouping
-        if inp_n.shape[1] % g_kernel != 0:
-            inp_n = np.pad(inp_n, ((0,0),(0,g_kernel-inp_n.shape[0]%g_kernel),(0,0),(0,0)))
-        # Transpose the array to match direction
         inp_n = np.transpose(inp_n, (2,3,1,0))
-    # Code modified from https://stackoverflow.com/questions/42297115/numpy-split-cube-into-cubes/42298440#42298440
     elif group_direction == 2: # FX Mode
-        # TODO : Prevent overflow caused by mapping next kernel map
-        # Pad Array to have more values
-        if inp_n.shape[2] % 3 != 0 or inp_n.shape[3] % 3 != 0:
-            inp_n = np.pad(inp_n, ((0,0),(0,0),(0,3-inp_n.shape[2]%3),(0,3-inp_n.shape[3]%3)))    
-        # Reshape to corresponding size to manipulate easier
-        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]//3, 3, inp_n.shape[3]//3, 3))
-        # Transpose to have 3x3 structure is preserved
-        print(inp_n.shape)
+        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]//CONF_SZ, CONF_SZ, inp_n.shape[3]//CONF_SZ, CONF_SZ))
         inp_n = inp_n.transpose((0,2,4,6,5,7,1,3))
     elif group_direction == 3: # FY Mode
-        # TODO : Prevent overflow caused by mapping next kernel map
-        # Pad Array to have more values
-        if inp_n.shape[2] % 3 != 0 or inp_n.shape[3] % 3 != 0:
-            inp_n = np.pad(inp_n, ((0,0),(0,0),(0,3-inp_n.shape[0]%3),(0,3-inp_n.shape[1]%3)))
-        # Reshape to corresponding size to manipulate easier
-        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]//3, 3, inp_n.shape[3]//3, 3))
-        # Transpose to have 3x3 structure is preserved
-        inp_n = inp_n.transpose((2,0,4,6,5,7,1,3))
+        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]/CONF_SZ3, CONF_SZ, inp_n.shape[3]//CONF_SZ, CONF_SZ))
+        inp_n = inp_n.transpose((0,2,6,4,5,7,1,3))
     elif group_direction == 4:
-        # TODO : Prevent overflow caused by mapping next kernel map
-        # Pad Array to have more values
-        if inp_n.shape[1] % g_kernel != 0 or inp_n.shape[2] % 3 != 0 or inp_n.shape[3] % 3 != 0:
-            inp_n = np.pad(inp_n, ((0,0),(0,0),(0,3-inp_n.shape[0]%3),(0,3-inp_n.shape[1]%3)))
-        # Reshape to corresponding size to manipulate easier
-        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]//3, 3, inp_n.shape[3]//3, 3))
-        # Transpose to have 3x3 structure is preserved
-        inp_n = inp_n.transpose((4,0,2,6,5,7,1,3))
+        inp_n = inp_n.reshape((inp_n.shape[0], 1, inp_n.shape[1], 1, inp_n.shape[2]//CONF_SZ, CONF_SZ, inp_n.shape[3]//CONF_SZ, CONF_SZ))
+        inp_n = inp_n.transpose((0,4,6,2,5,7,1,3))
     else:
         raise ValueError("group_direction not supported")
     # Save modified shape
     inp_m_shape = inp_n.shape
-        
+
+    # STEP 2 : make groups and adjust mantissa
     # Convert to byte stream
     st = inp_n.tobytes() 
     # Set to uint32 array to easy computing
@@ -173,9 +92,8 @@ def _make_groups_tensor(inp, group_mantissa, group_size, group_direction):
     # Get the max value
     # IDEA : send shift code to back, maybe that's faster
     np.right_shift(e_, 23, out=e_)
-    # Match shape to divisible to group size <- maybe this is not necessary because of pre-processing
-    m_ = np.append(e_, np.zeros(group_size - e_.shape[0] % group_size, dtype=np.uint32))
-    m_ = np.reshape(m_, (group_size, -1))
+    # Reshape arrat to group size to get max values
+    m_ = np.reshape(e_, (group_size, -1))
     # get the max value of each blocks
     m_ = np.amax(m_, axis=0)
     # Revert back to original size
@@ -186,16 +104,18 @@ def _make_groups_tensor(inp, group_mantissa, group_size, group_direction):
     # On hardware, mantissa bits have to store the 1 from the IEEE Standard
     e_ = group_mantissa - (m_ - e_) - 1
     r_mask = np.full(v.shape, 0x007fffff, dtype=np.uint32)
-    # When mantissa have to shift more than precision bits, total value have to be zero.
+    # When mantissa have to shift more than precision bits, total value is zero on hardware.
     r_mask[e_ > 0xff] = 0xffffffff
-    # Clip the negative value
-    # Maybe more smarter way...? -> np.clip(e_, 0, 0xff, out=e_)
+    # Clip the negative value, Maybe more smarter way...? -> np.clip(e_, 0, 0xff, out=e_)
     e_[e_ > 0xff] = 0
     # Shift to make reversed mask
     np.right_shift(r_mask, e_, out=r_mask)
     # Get the reversed mask
     np.invert(r_mask, out=r_mask)
+    # appling mask to data
     r_ = np.bitwise_and(v, r_mask)
+
+    # STEP 3 : reverting array
     # revert to original np.float32 
     r = np.frombuffer(r_, dtype=np.float32)
     # revert back to original shape
@@ -207,22 +127,15 @@ def _make_groups_tensor(inp, group_mantissa, group_size, group_direction):
         r = np.transpose(r, (3,2,0,1))
     elif group_direction == 2: # FX
         r = r.transpose((0,6,1,7,2,4,3,5))
-        print(r.shape)
-        r = r.reshape(inp_shape)
+        r = r.reshape(inp_p_shape)
     elif group_direction == 3: # FY Mode
-        r = r.transpose((1,6,0,7,2,4,3,5))
-        r = r.reshape(inp_shape)
+        r = r.transpose((0,6,1,7,3,4,2,5))
+        r = r.reshape(inp_p_shape)
     elif group_direction == 4:
-        r = r.transpose((1,6,2,7,0,4,3,5))
-        r = r.reshape(inp_shape)
-    else:
-        raise ValueError("group_direction not supported")
-
-    print(r.shape)
+        r = r.transpose((0,6,3,7,1,4,2,5))
+        r = r.reshape(inp_p_shape)
     # Revert padding
-    # Cut the array
-    # r = r[:(orig_shape[0]%3)-3,:(orig_shape[1]%3)-3,:,:]
-    if inp_m_shape != inp_shape:
-        pass
-    
+    if inp_p_shape != inp_shape:
+        r = r[:inp_shape[0],:inp_shape[1],:inp_shape[2],:inp_shape[3]]
+
     return torch.from_numpy(r)
