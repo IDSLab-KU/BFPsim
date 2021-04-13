14088a21b9bcd9dcdad47d0030e0ef3adb921e1a
R3C0D3r <cryptographcode@gmail.com>
2021-04-13 20:17:32 +0900

    fix : ready for test executing

--- run.sh
+++ run.sh
@@ -1,28 +1,33 @@
-docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet --save True --stat True
 
-# Etc
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/_testbench.py
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet -bf AlexNet_4
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 -bf ResNet18_8
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet
 
-# Execute using docker
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime python3 -u /app/_test.py
+docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m VGG16
 
-# Running Simplenet
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m SimpleNet --stat True --save True
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m SimpleNet -bf SimpleNet_16 --stat True
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime python3 -u /app/main.py -m SimpleNet -bf SimpleNet_8 --stat True
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime python3 -u /app/main.py -m SimpleNet -bf SimpleNet_4 --stat True
 
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime python3 -u /app/main.py -m ResNet18 -bf ResNet18_4_OutX --stat True --save True
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 --stat True --save True
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 -bf ResNet18_8_54_X --stat True --save True
 
-# ResNet18 with others
-# docker run --rm --cpus="4" --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 --stat True --save True --training-epochs 50
-# docker run --rm --cpus="4" --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 --bf ResNet18_8_54_X --stat True --save True --training-epochs 50
-# docker run --rm --cpus="4" --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 --bf ResNet18_16_54_X --stat True --save True --training-epochs 50
 
-# Running zero-test simulation
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py --save-file ResNet18.model --mode zero-test --zt-bf False # --log False
+# Execute
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet -bf AlexNet_4 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet -bf AlexNet_8 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m AlexNet -bf AlexNet_16 --save True --stat True
+
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 -bf ResNet18_4 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 -bf ResNet18_8 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ResNet18 -bf ResNet18_16 --save True --stat True
+
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m VGG16 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m VGG16 -bf VGG16_4 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m VGG16 -bf VGG16_8 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m VGG16 -bf VGG16_16 --save True --stat True
+
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m MobileNetv1 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m MobileNetv1 -bf MobileNetv1_4 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m MobileNetv1 -bf MobileNetv1_8 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m MobileNetv1 -bf MobileNetv1_16 --save True --stat True
+
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m DenseNet --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m DenseNet -bf DenseNet_4 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m DenseNet -bf DenseNet_8 --save True --stat True
+# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m DenseNet -bf DenseNet_16 --save True --stat True
