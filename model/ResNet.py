a0a04e3f8b1d7ca4776b3392d3aabbbd339daaf7
R3C0D3r <cryptographcode@gmail.com>
2021-04-13 20:11:18 +0900

    fix : edit resnet config files

--- model/ResNet.py
+++ model/ResNet.py
@@ -43,19 +43,21 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        # self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.conv1 = SetConv2dLayer("conv1", bf_conf, in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         # self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.conv2 = SetConv2dLayer("conv2", bf_conf, planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)        
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)   
+        # self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.conv3 = SetConv2dLayer("conv3", bf_conf, planes, self.expansion * planes, kernel_size=1, bias=False)   
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
+                # nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                SetConv2dLayer("shortcut", bf_conf, pin_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
@@ -65,44 +67,6 @@ class Bottleneck(nn.Module):
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
         out = F.relu(out)
-        return out
-
-
-class BFResNet(nn.Module):
-    def __init__(self, block, num_blocks, bf_conf, num_classes):
-        super(BFResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0],
-            bf_conf=bf_conf["layer1"], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1],
-            bf_conf=bf_conf["layer2"], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2],
-            bf_conf=bf_conf["layer3"], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3],
-            bf_conf=bf_conf["layer4"], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, bf_conf, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for i in range(len(strides)):
-            layers.append(block(self.in_planes, planes, bf_conf[str(i)], strides[i]))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
         return out
 
 class ResNet(nn.Module):
