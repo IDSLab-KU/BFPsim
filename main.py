2c086a9dddd77af8fb58c2487da9290e7ac5dc2c
R3C0D3r <cryptographcode@gmail.com>
2021-04-15 04:27:21 +0900

    fix : migrating code to utils

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 
 from log import Log
 from functions import LoadDataset, BFConf, Stat, str2bool
+from utils import ZeroTest, SaveData
 
 from model.AlexNet import AlexNet
 from model.ResNet import ResNet18
@@ -55,7 +56,7 @@ def ArgumentParse():
 
     """Train mode"""
     # Data loader
-    parser.add_argument("-d","--dataset", type=str, default = "CIFAR-10",
+    parser.add_argument("-d","--dataset", type=str, default = "CIFAR10",
         help = "Dataset to use [CIFAR10, CIFAR100]")
     parser.add_argument("-nw","--num-workers", type=int, default = 4,
         help = "Number of workers to load data")
@@ -320,77 +321,6 @@ def TrainNetwork():
         SaveModel("finish")
 
 
-from blockfunc import GetZeroSettingError
-import numpy as np
-from functions import SaveStackedGraph
-
-def ZeroTest():
-
-    Log.SetPrintCurrentTime(False)
-    Log.SetPrintElapsedTime(False)
-
-    parameters = torch.load(args.save_file).items()
-    
-    layer_count = 0
-    layer_list = []
-    layer_list_short = []
-    Log.Print("List of weight data's name")
-    for name, param in parameters:
-        n = name.split(".")
-        # Normal model name convention
-        if args.model == "ResNet18" and args.zt_bf == False:
-            condition = "layer" in n[1] and "conv" in n[3] and "weight" in n[4]
-            if condition:
-                short_name = n[1]+"_"+n[2]+"_"+n[3]
-        elif args.model == "ResNet18" and args.zt_bf == True:
-            condition = "layer" in n[0] and "conv" in n[2] and "weight" in n[3]
-            if condition:
-                short_name = n[0]+"_"+n[1]+"_"+n[2]
-        else:
-            raise NotImplementedError("model and layer conf not implemented")
-
-        if condition:
-            Log.Print("{}({})".format(name, param.size()))
-            layer_list.append(name)
-            layer_list_short.append(short_name)
-            layer_count += 1
-        
-    Log.Print("")
-
-    # for bits in [4]:
-    #     for g_size in [36]:
-    for bits in [4, 5, 6, 7, 8]:
-        for g_size in [36, 54, 72]:
-            stat_data = np.zeros((layer_count, bits+1),dtype=np.int32)
-            Log.Print("Mantissa bits {}, Group size {}".format(bits, g_size))
-            res = np.zeros(bits+1, dtype=np.int32)
-            ind = 0
-            for name, param in parameters:
-                if name in layer_list:
-                    d = GetZeroSettingError(param.detach(), bits, g_size, 0)
-                    res += d
-                    stat_data[ind] = d
-                    ind += 1
-
-                    if args.zt_print_mode == "format":
-                        for i in d:
-                            Log.Print("{}".format(i),end="\t")
-                        Log.Print("")
-                    if args.zt_print_mode == "all":
-                        Log.Print("[{:7.3f}%]{:8d}/{:8d}, {}".format(d[-1]/(d.sum())*100,d[-1],d.sum(),d))
-            # Save figures
-            if args.zt_graph_mode != "none":
-                SaveStackedGraph(layer_list_short, np.flip(stat_data.transpose(),axis=0),
-                        mode=args.zt_graph_mode,
-                        title="{}, Bit={}, Group={}".format(args.model, bits, g_size),
-                        save="{}_{}_{}".format(args.model, bits, g_size))
-            if args.zt_print_mode in ["sum", "format", "all"]:
-                Log.Print("[{:7.3f}%]{:10d}/{:10d}, {}".format(res[-1]/(res.sum())*100,res[-1],res.sum(),res))            
-                Log.Print("")
-    # print(args.net)
-
-
-
 if __name__ == '__main__':
     # handle signal
     signal.signal(signal.SIGINT, handler)
@@ -415,5 +345,10 @@ if __name__ == '__main__':
         Log.Print("Loaded saved file: {}".format(args.save_file), current=False, elapsed=False)
         Log.Print("Graph mode: {}".format(args.zt_graph_mode), current=False, elapsed=False)
         Log.Print("Print mode: {}".format(args.zt_print_mode), current=False, elapsed=False)
-        ZeroTest()
-        
+        ZeroTest(args)
+    elif args.mode == "save-data":
+        Log.Print("Program executed on save-data mode.", current=False, elapsed=False)
+        Log.Print("Loaded saved file: {}".format(args.save_file), current=False, elapsed=False)
+        SaveData(args)
+    else:
+        raise NotImplementedError("Mode not supported : {}".format(args.mode))
---
+++ utils.py
@@ -0,0 +1,85 @@
+# utils.py
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+
+from log import Log
+from functions import BFConf, Stat, str2bool
+
+
+from blockfunc import GetZeroSettingError
+import numpy as np
+from functions import SaveStackedGraph
+
+
+
+def SaveData(args):
+    pass
+
+
+
+def ZeroTest(args):
+
+    Log.SetPrintCurrentTime(False)
+    Log.SetPrintElapsedTime(False)
+
+    parameters = torch.load(args.save_file).items()
+    
+    layer_count = 0
+    layer_list = []
+    layer_list_short = []
+    Log.Print("List of weight data's name")
+    for name, param in parameters:
+        n = name.split(".")
+        # Normal model name convention
+        if args.model == "ResNet18" and args.zt_bf == False:
+            condition = "layer" in n[1] and "conv" in n[3] and "weight" in n[4]
+            if condition:
+                short_name = n[1]+"_"+n[2]+"_"+n[3]
+        elif args.model == "ResNet18" and args.zt_bf == True:
+            condition = "layer" in n[0] and "conv" in n[2] and "weight" in n[3]
+            if condition:
+                short_name = n[0]+"_"+n[1]+"_"+n[2]
+        else:
+            raise NotImplementedError("model and layer conf not implemented")
+
+        if condition:
+            Log.Print("{}({})".format(name, param.size()))
+            layer_list.append(name)
+            layer_list_short.append(short_name)
+            layer_count += 1
+        
+    Log.Print("")
+
+    # for bits in [4]:
+    #     for g_size in [36]:
+    for bits in [4, 5, 6, 7, 8]:
+        for g_size in [36, 54, 72]:
+            stat_data = np.zeros((layer_count, bits+1),dtype=np.int32)
+            Log.Print("Mantissa bits {}, Group size {}".format(bits, g_size))
+            res = np.zeros(bits+1, dtype=np.int32)
+            ind = 0
+            for name, param in parameters:
+                if name in layer_list:
+                    d = GetZeroSettingError(param.detach(), bits, g_size, 0)
+                    res += d
+                    stat_data[ind] = d
+                    ind += 1
+
+                    if args.zt_print_mode == "format":
+                        for i in d:
+                            Log.Print("{}".format(i),end="\t")
+                        Log.Print("")
+                    if args.zt_print_mode == "all":
+                        Log.Print("[{:7.3f}%]{:8d}/{:8d}, {}".format(d[-1]/(d.sum())*100,d[-1],d.sum(),d))
+            # Save figures
+            if args.zt_graph_mode != "none":
+                SaveStackedGraph(layer_list_short, np.flip(stat_data.transpose(),axis=0),
+                        mode=args.zt_graph_mode,
+                        title="{}, Bit={}, Group={}".format(args.model, bits, g_size),
+                        save="{}_{}_{}".format(args.model, bits, g_size))
+            if args.zt_print_mode in ["sum", "format", "all"]:
+                Log.Print("[{:7.3f}%]{:10d}/{:10d}, {}".format(res[-1]/(res.sum())*100,res[-1],res.sum(),res))            
+                Log.Print("")
+    # print(args.net)
