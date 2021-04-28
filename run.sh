d126a2d52bb017300ef5102eb5920b8371ff0e93
R3C0D3r <cryptographcode@gmail.com>
2021-04-28 13:49:10 +0900

    docs : additional config, run file fix

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ BIT=M2
 
 # Save with savefile name
 # docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ${MODEL} --dataset ${DATASET} --save True --stat True --save-name ${MODEL}_${DATASET}_FP32
-# docker run --rm --gpus '"device=0"' --workdir /app -v "$(pwd)":/app 4bd4764d9367 python3 -u /app/main.py -m ${MODEL} -bf ${MODEL}_${BIT} --dataset ${DATASET} --save True --stat True --save-name ${MODEL}_${DATASET}_${BIT}bit
+# docker run --rm --gpus '"device=2"' --workdir /app -v "$(pwd)":/app ff60c1c32cb7 python3 -u /app/main.py -m ${MODEL} -bf ${MODEL}_${BIT} --dataset ${DATASET} --save True --stat True --save-name ${MODEL}_${DATASET}_${BIT}bit
 
 # for MODEL in AlexNet ResNet18 DenseNetCifar MobileNetv1 VGG16
 # do
