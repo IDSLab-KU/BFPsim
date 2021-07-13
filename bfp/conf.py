12a91d71438433a4e45786957ddb3d526f52bbac
R3C0D3r <coder@dgist.ac.kr>
2021-07-13 08:16:47 +0000

    feat: config can accept human understanble config

--- bfp/conf.py
+++ bfp/conf.py
@@ -1,55 +1,88 @@
 
 
+# TODO : improve dimension loading code
+# "wi, 36" returns (4,1,3,3)
+# [1,4,3,3] returns original stuffs
+# single number with 1 returns (1,1,1,1)
+def GetDimension(val):
+    if type(val) == list:
+        return val
+    elif type(val) == str:
+        v = val.replace(" ","").split(",")
+        gs = int(v[1])
+        if v[0].lower() == "wi":
+            return (gs//9,1,3,3)
+        elif v[0].lower() == "wo":
+            return (1,gs//9,3,3)
+        elif v[0].lower() == "fx":
+            return (1,1,gs//3,3)
+        elif v[0].lower() == "fy":
+            return (1,1,3,gs//3)
+        elif v[0].lower() == "fc":
+            return (1,gs//9,3,3)
+        else:
+            ValueError("BFPConf direction is not recognized: {v[0]}")
+    elif val == 1:
+        return (1,1,1,1)
+
+def GetTupleShortString(t):
+    s = "("
+    for i in t:
+        s +="%d,"%i
+    s = s[:-1]
+    s += ")"
+    return s
+
 class BFPConf():
     def __init__(self, dic=None, bwg_boost = 1.0):
         if dic == None:
             dic = dict()
-        self.type   = dic["type"]              if "type"   in dic.keys() else "Conv2d"
+        self.type   = dic["type"]                   if "type"   in dic.keys() else "Conv2d"
 
         # Foward - Weight
-        self.fw     = dic["fw"]                if "fw"     in dic.keys() else True
-        self.fw_bit = dic["fw_bit"]            if "fw_bit" in dic.keys() else 8
-        self.fw_dim  = tuple(dic["fw_dim"])    if "fw_dim" in dic.keys() else (1,16,3,3)
+        self.fw     = dic["fw"]                         if "fw"     in dic.keys() else True
+        self.fw_bit = dic["fw_bit"]                     if "fw_bit" in dic.keys() else 8
+        self.fw_dim  = GetDimension(dic["fw_dim"])      if "fw_dim" in dic.keys() else (1,16,3,3)
 
         # Forward - Input
-        self.fi     = dic["fi"]                if "fi"     in dic.keys() else True
-        self.fi_bit = dic["fi_bit"]            if "fi_bit" in dic.keys() else self.fw_bit
-        self.fi_dim  = tuple(dic["fi_dim"])    if "fi_dim" in dic.keys() else (1,4,3,3)
+        self.fi     = dic["fi"]                         if "fi"     in dic.keys() else True
+        self.fi_bit = dic["fi_bit"]                     if "fi_bit" in dic.keys() else self.fw_bit
+        self.fi_dim  = GetDimension(dic["fi_dim"])      if "fi_dim" in dic.keys() else (1,4,3,3)
 
         # Forward - Output
-        self.fo     = dic["fo"]                if "fo"     in dic.keys() else False
-        self.fo_bit = dic["fo_bit"]            if "fo_bit" in dic.keys() else self.fw_bit
-        self.fo_dim  = tuple(dic["fo_dim"])    if "fo_dim" in dic.keys() else (1,4,3,3)
+        self.fo     = dic["fo"]                         if "fo"     in dic.keys() else False
+        self.fo_bit = dic["fo_bit"]                     if "fo_bit" in dic.keys() else self.fw_bit
+        self.fo_dim  = GetDimension(dic["fo_dim"])      if "fo_dim" in dic.keys() else (1,4,3,3)
 
         # Backward - Output gradient while calculating input gradient
-        self.bio     = dic["bio"]                if "bio"     in dic.keys() else True
-        self.bio_bit = dic["bio_bit"]            if "bio_bit" in dic.keys() else self.fo_bit
-        self.bio_dim  = tuple(dic["bio_dim"])    if "bio_dim" in dic.keys() else self.fo_dim
+        self.bio     = dic["bio"]                       if "bio"     in dic.keys() else True
+        self.bio_bit = dic["bio_bit"]                   if "bio_bit" in dic.keys() else self.fo_bit
+        self.bio_dim  = GetDimension(dic["bio_dim"])    if "bio_dim" in dic.keys() else self.fo_dim
 
         # Backward - Weight while calculating input gradient
-        self.biw     = dic["biw"]                if "biw"     in dic.keys() else True
-        self.biw_bit = dic["biw_bit"]            if "biw_bit" in dic.keys() else self.fw_bit
-        self.biw_dim  = tuple(dic["biw_dim"])    if "biw_dim" in dic.keys() else self.fw_dim
+        self.biw     = dic["biw"]                       if "biw"     in dic.keys() else True
+        self.biw_bit = dic["biw_bit"]                   if "biw_bit" in dic.keys() else self.fw_bit
+        self.biw_dim  = GetDimension(dic["biw_dim"])    if "biw_dim" in dic.keys() else self.fw_dim
 
         # Backward - Calculated input gradient
-        self.big     = dic["big"]                if "big"     in dic.keys() else False
-        self.big_bit = dic["big_bit"]            if "big_bit" in dic.keys() else self.fi_bit
-        self.big_dim  = tuple(dic["big_dim"])    if "big_dim" in dic.keys() else self.fi_dim
+        self.big     = dic["big"]                       if "big"     in dic.keys() else False
+        self.big_bit = dic["big_bit"]                   if "big_bit" in dic.keys() else self.fi_bit
+        self.big_dim  = GetDimension(dic["big_dim"])    if "big_dim" in dic.keys() else self.fi_dim
 
         # Backward - Output gradient while calculating weight gradient
-        self.bwo     = dic["bwo"]                if "bwo"     in dic.keys() else True
-        self.bwo_bit = dic["bwo_bit"]            if "bwo_bit" in dic.keys() else self.fo_bit
-        self.bwo_dim  = tuple(dic["bwo_dim"])    if "bwo_dim" in dic.keys() else (1,1,12,3)
+        self.bwo     = dic["bwo"]                       if "bwo"     in dic.keys() else True
+        self.bwo_bit = dic["bwo_bit"]                   if "bwo_bit" in dic.keys() else self.fo_bit
+        self.bwo_dim  = GetDimension(dic["bwo_dim"])    if "bwo_dim" in dic.keys() else (1,1,12,3)
 
         # Backward - Input while calculating weight gradient
-        self.bwi     = dic["bwi"]                if "bwi"     in dic.keys() else True
-        self.bwi_bit = dic["bwi_bit"]            if "bwi_bit" in dic.keys() else self.fi_bit
-        self.bwi_dim  = tuple(dic["bwi_dim"])    if "bwi_dim" in dic.keys() else (1,1,12,3)
+        self.bwi     = dic["bwi"]                       if "bwi"     in dic.keys() else True
+        self.bwi_bit = dic["bwi_bit"]                   if "bwi_bit" in dic.keys() else self.fi_bit
+        self.bwi_dim  = GetDimension(dic["bwi_dim"])    if "bwi_dim" in dic.keys() else (1,1,12,3)
 
         # Backward - Calculated weight gradient
-        self.bwg     = dic["bwg"]                if "bwg"     in dic.keys() else False
-        self.bwg_bit = dic["bwg_bit"]            if "bwg_bit" in dic.keys() else self.fw_bit
-        self.bwg_dim  = tuple(dic["bwg_dim"])    if "bwg_dim" in dic.keys() else (1,1,12,3)
+        self.bwg     = dic["bwg"]                       if "bwg"     in dic.keys() else False
+        self.bwg_bit = dic["bwg_bit"]                   if "bwg_bit" in dic.keys() else self.fw_bit
+        self.bwg_dim  = GetDimension(dic["bwg_dim"])    if "bwg_dim" in dic.keys() else (1,1,12,3)
 
         self.bwg_boost = bwg_boost
 
@@ -89,15 +122,15 @@ class BFPConf():
         if self.fw_dim == self.fi_dim == self.fo_dim == self.bio_dim == self.biw_dim == self.big_dim == self.bwo_dim == self.bwi_dim == self.bwg_dim:
             s += '{}'.format(self.fw_dim)
         else:
-            s += "[{}/".format(self.fw_dim) if self.fw else "[_/" 
-            s += "{}/".format(self.fi_dim) if self.fi else "_/" 
-            s += "{}/".format(self.fo_dim) if self.fo else "_/" 
-            s += "{}/".format(self.bio_dim) if self.bio else "_/" 
-            s += "{}/".format(self.biw_dim) if self.biw else "_/" 
-            s += "{}/".format(self.big_dim) if self.big else "_/" 
-            s += "{}/".format(self.bwo_dim) if self.bwo else "_/" 
-            s += "{}/".format(self.bwi_dim) if self.bwi else "_/" 
-            s += "{}".format(self.bwg_dim)  if self.bwg else "_]"
+            s += "[{}/".format(GetTupleShortString(self.fw_dim)) if self.fw else "[_/" 
+            s += "{}/".format(GetTupleShortString(self.fi_dim)) if self.fi else "_/" 
+            s += "{}/".format(GetTupleShortString(self.fo_dim)) if self.fo else "_/" 
+            s += "{}/".format(GetTupleShortString(self.bio_dim)) if self.bio else "_/" 
+            s += "{}/".format(GetTupleShortString(self.biw_dim)) if self.biw else "_/" 
+            s += "{}/".format(GetTupleShortString(self.big_dim)) if self.big else "_/" 
+            s += "{}/".format(GetTupleShortString(self.bwo_dim)) if self.bwo else "_/" 
+            s += "{}/".format(GetTupleShortString(self.bwi_dim)) if self.bwi else "_/" 
+            s += "{}".format(GetTupleShortString(self.bwg_dim))  if self.bwg else "_]"
         if self.bwg_boost != 1.0:
             s += ",bwg_boost={}".format(self.bwg_boost)
         return s
