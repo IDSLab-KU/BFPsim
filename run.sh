896012496b0f4a4a5995235abeed6a0b0f62dd49
R3C0D3r <coder@dgist.ac.kr>
2021-10-30 20:19:58 +0000

    style: updated executing shell

--- run.sh
+++ run.sh
@@ -13,7 +13,9 @@
 
 # docker run -it $(whoami)/bfpsim:latest /bin/bash
 
-docker run --rm --gpus '"device=3"' --cpus="8" --user "$(id -u):$(id -g)" --mount type=bind,source=/dataset,target=/dataset --shm-size 24G --workdir /app -v "$(pwd)":/app $(whoami)/bfpsim:latest python3 -u /app/main.py --mode train --model ResNet18 --dataset CIFAR100 --log True --bfp ResNet18_FB24
+docker run --rm --gpus '"device=2"' --cpus="8" --user "$(id -u):$(id -g)" --mount type=bind,source=/dataset,target=/dataset --shm-size 24G --workdir /app -v "$(pwd)":/app $(whoami)/bfpsim:latest python3 -u /app/main.py --mode train --model ResNet18 --dataset CIFAR100 --log True --bfp ResNet18_FB24
+
+# docker run --rm --gpus '"device=3"' --cpus="8" --user "$(id -u):$(id -g)" --mount type=bind,source=/dataset,target=/dataset --shm-size 24G --workdir /app -v "$(pwd)":/app $(whoami)/bfpsim:latest python3 -u /app/main.py --mode train --model ResNet18 --dataset CIFAR100 --log True
 
 # Run tensorboard
 # docker run --rm --gpus '"device=3"' --cpus="8" --user "$(id -u):$(id -g)" --mount type=bind,source=/dataset,target=/dataset --shm-size 24G -p 8088:8088 -d --name=$(whoami)/bfpsim/tensorboard --workdir /app -v "$(pwd)":/app $(whoami)/bfpsim:latest tensorboard --logdir=runs --port 8088 --host 0.0.0.0
