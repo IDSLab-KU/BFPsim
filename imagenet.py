4b097ffdfd3f3e74f8c60031c78718d02a7276eb
r3coder <cryptographcode@gmail.com>
2021-11-25 15:48:13 +0900

    fix: changing imagenet save file path location

--- imagenet.py
+++ imagenet.py
@@ -48,7 +48,7 @@ parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=64, type=int,
+parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
@@ -448,7 +448,7 @@ def validate(val_loader, model, criterion, args):
 def save_checkpoint(args, state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, args.save_prefix + "/" + filename)
     if is_best:
-        shutil.copyfile(args.save_prefix + "/" + filename, 'model_best.pth.tar')
+        shutil.copyfile(args.save_prefix + "/" + filename, args.save_prefix + "/" + 'model_best.pth.tar')
 
 
 class AverageMeter(object):
