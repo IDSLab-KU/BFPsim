0f2eebe9cfc3b322e890c4a0b9f0e0a0eb297e23
r3coder <cryptographcode@gmail.com>
2021-11-25 16:57:50 +0900

    feat: linear layer implemented

--- bfp/module.py
+++ bfp/module.py
@@ -25,40 +25,43 @@ _size_1_t = _scalar_or_tuple_1_t[int]
 _size_2_t = _scalar_or_tuple_2_t[int]
 _size_3_t = _scalar_or_tuple_3_t[int]
 
-from bfp.internal import make_groups_tensor
+from bfp.internal import make_groups_tensor, gradient_linear_weight
 from bfp.conf import BFPConf
 
 # BlockFloat Linear Function
 class BFPLinearFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, weight, bias, bfp_conf):    
+    def forward(ctx, input, weight, bias=None, bfp_conf=None):
+        # print("input:", input.shape)
         # Grouping input and weight
+        
         if bfp_conf.fi:
-            input = make_groups_tensor(input, bfp_conf.fi_bit, bfp_conf.fi_dim)
+            input_ = make_groups_tensor(input.clone().detach(), bfp_conf.fi_bit, bfp_conf.fi_dim)
         if bfp_conf.fw:
-            weight = make_groups_tensor(weight, bfp_conf.fw_bit, bfp_conf.fw_dim)
+            weight_ = make_groups_tensor(weight.clone().detach(), bfp_conf.fw_bit, bfp_conf.fw_dim)
+
         # Save context to use on backward
         ctx.bfp_conf = bfp_conf
-        ctx.save_for_backward(input, weight, bias)
+        ctx.save_for_backward(input_, weight_, bias)
         
-        # Compute FC and return
-        output = F.linear(input, weight, bias)
+        if bias != None:
+            output = F.linear(input_, weight_, bias)
+        else:
+            output = F.linear(input_, weight_)
         if bias is not None:
             output += bias.unsqueeze(0).expand_as(output)
 
         # Grouping Output
         if bfp_conf.fo:
             output = make_groups_tensor(output, bfp_conf.fo_bit, bfp_conf.fo_dim)
-        print("F: %s %s %s"%(input.shape, weight.shape, output.shape))
+
         return output
     
     @staticmethod
     def backward(ctx, grad_output):
         # Load saved tensors
-        # input, weight, bias, confs = ctx.saved_tensors
         input, weight, bias = ctx.saved_tensors
         bfp_conf = ctx.bfp_conf
-        print("output gradient")
 
         # Calculate gradients
         grad_input = grad_weight = grad_bias = None
@@ -66,91 +69,60 @@ class BFPLinearFunction(torch.autograd.Function):
         # Calculate Input Gradient
         ## Grouping grad_output
         if bfp_conf.bio:
-            grad_output_ = make_groups_tensor(grad_output, bfp_conf.bio_bit, bfp_conf.bio_dim)
+            grad_output_ = make_groups_tensor(grad_output.clone().detach(), bfp_conf.bio_bit, bfp_conf.bio_dim)
         else: # Apply original gradient if grad_output is not grouped
             grad_output_ = grad_output
         ## Grouping weight
         if bfp_conf.biw:
-            weight = make_groups_tensor(weight, bfp_conf.biw_bit, bfp_conf.biw_dim)        
+            weight_ = make_groups_tensor(weight.clone().detach(), bfp_conf.biw_bit, bfp_conf.biw_dim)
+        else:
+            weight_ = weight
 
-        print(grad_output_.shape)
-        print(weight.shape)
-        grad_input_ = F.linear(grad_output_, weight.t(), bias).t()
-        print(grad_input.shape)
-        # grad_input_ = grad_output_.mm(weight)
-
+        grad_input = F.linear(grad_output_, weight_.t())
+        
         if bfp_conf.big:
-            grad_input_ = make_groups_tensor(grad_input, bfp_conf.big_bit,bfp_conf.big_dim)
-        else: # If not grouping, use original type
+            grad_input_ = make_groups_tensor(grad_input.clone().detach(), bfp_conf.big_bit,bfp_conf.big_dim)
+        else:
             grad_input_ = grad_input
 
         if bfp_conf.bwo:
             # Regroup if bwo / bio grouping configuration is different!
             if (bfp_conf.bwo_bit != bfp_conf.bio_bit or bfp_conf.bwo_dim != bfp_conf.bio_dim):
-                grad_output_ = make_groups_tensor(grad_output, bfp_conf.bwo_bit, bfp_conf.bwo_dim)
+                grad_output_ = make_groups_tensor(grad_output.clone().detach(), bfp_conf.bwo_bit, bfp_conf.bwo_dim)
+            else:
+                grad_output_ = grad_output
         else: # If not grouping, use original type
             grad_output_ = grad_output
-        ## Grouping input - it's not grad_input, right?
+        ## Grouping input
         if bfp_conf.bwi:
             # Regroup if bwi / fi grouping configuration is different!
             if (bfp_conf.bwi_bit != bfp_conf.fi_bit or bfp_conf.bwi_dim != bfp_conf.fi_dim):
-                input = make_groups_tensor_fc(input, bfp_conf.bwi_bit, bfp_conf.bwi_dim)
-        grad_weight = F.linear(grad_output_.t(), input.t(), bias).t()
-        # grad_weight = grad_output_.t().mm(input)
-        # print(grad_weight.shape)
-        # Group the gradient of weight
-        if bfp_conf.bwg:
-            grad_weight = make_groups_tensor_fc(grad_weight, bfp_conf.bwg_bit, bfp_conf.bwg_dim)
+                input_ = make_groups_tensor(input.clone().detach(), bfp_conf.bwi_bit, bfp_conf.bwi_dim)
+            else:
+                input_ = input
+        else:
+            input_ = input
+        
 
+        # grad_weight = torch.empty((grad_output.shape[2], input.shape[2]),dtype=torch.float).cuda()
+        grad_weight = gradient_linear_weight(grad_output_.clone().detach(), input_.clone().detach(), weight.shape)
+        # Group the gradient of weight
+        
+        if bfp_conf.bwg:
+            grad_weight = make_groups_tensor(grad_weight.clone().detach(), bfp_conf.bwg_bit, bfp_conf.bwg_dim)
+        else:
+            grad_weight = grad_weight
+            
         if bfp_conf.bwg_boost != 1.0:
             grad_weight /= bfp_conf.bwg_boost
 
         if bias is not None:
             grad_bias = grad_output.sum(0)
 
-        return grad_input_, grad_weight, grad_bias, None
-
-# Blockfloat Linear
-class BFPLinear(torch.nn.Module):
-    def __init__(self,
-                input_features: int,
-                output_features: int,
-                bfp_conf: BFPConf,
-                bias=True):
-        super(BFPLinear, self).__init__()
-        self.input_features = input_features
-        self.output_features = output_features
-        self.bfp_conf = bfp_conf
-
-        # Weight parameters, should be grouped with few numbers
-        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
-
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(output_features))
-        else:
-            self.register_paramter('bias', None)
-        
-        # Initialize weights manually
-        self.weight.data.uniform_(-0.1, 0.1)
-        if self.bias is not None:
-            self.bias.data.uniform_(-0.1, 0.1)
-    
-    def forward(self, input):
-        return BFPLinearFunction.apply(input, self.weight, self.bias, self.bfp_conf)
-    
-    def extra_repr(self):
-        s = ('{input_features}, {output_features}')
-        s += ', bfp_conf=({bfp_conf})'
-        if self.bias is None:
-            s += ', bias=False'
-        else:
-            s += ', bias=True'
-        return s.format(**self.__dict__)
-
+        return grad_input_, grad_weight_, grad_bias, None
 
 from torch.utils.cpp_extension import load
 cudnn_convolution = load(name="cudnn_convolution", sources=["./extensions/cudnn_convolution.cpp"], build_directory= "./extensions/", verbose=True)
-
 
 # Blockfloat Convolution Function
 # TODO : Implement Conv2d Operation
@@ -215,9 +187,12 @@ class BFPConv2dFunction(torch.autograd.Function):
         if ctx.needs_input_grad[0]: # First Layer's grad_input will be None
             # grad_input = torch.nn.grad.conv2d_input(input.shape, weight_, grad_output_, stride, padding, dilation, groups)
             grad_input = cudnn_convolution.convolution_backward_input(input.shape, weight_, grad_output_, stride, padding, dilation, groups, False, False, False)
+
         ## Grouping output grad_input
         if bfp_conf.big and grad_input != None:
-            make_groups_tensor(grad_input, bfp_conf.big_bit,bfp_conf.big_dim, 12)
+            grad_input_ = make_groups_tensor(grad_input, bfp_conf.big_bit,bfp_conf.big_dim, 12)
+        else:
+            grad_input_ = grad_input
 
         # Calculate Weight Gradient (2D Convolution, Depthwise Convolution)
         ## Grouping grad_output
@@ -232,14 +207,21 @@ class BFPConv2dFunction(torch.autograd.Function):
         if bfp_conf.bwi:
             # Regroup if bwi / fi grouping configuration is different!
             if (bfp_conf.bwi_bit != bfp_conf.fi_bit or bfp_conf.bwi_dim != bfp_conf.fi_dim):
-                make_groups_tensor(input, bfp_conf.bwi_bit, bfp_conf.bwi_dim, 21)
+                input_ = make_groups_tensor(input, bfp_conf.bwi_bit, bfp_conf.bwi_dim, 21)
+            else:
+                input_ = input
+        else:
+            input_ = input
+        
         ## Do the convolution
         if ctx.needs_input_grad[1]:
             # grad_weight = torch.nn.grad.conv2d_weight(input, weight.shape, grad_output_, stride, padding, dilation, groups)
-            grad_weight = cudnn_convolution.convolution_backward_weight(input, weight.shape, grad_output_, stride, padding, dilation, groups, False, False, False)
+            grad_weight = cudnn_convolution.convolution_backward_weight(input_, weight.shape, grad_output_, stride, padding, dilation, groups, False, False, False)
         # Group the gradient of weight
         if bfp_conf.bwg and grad_weight != None:
-            make_groups_tensor(grad_weight, bfp_conf.bwg_bit, bfp_conf.bwg_dim, 22)
+            grad_weight_ = make_groups_tensor(grad_weight, bfp_conf.bwg_bit, bfp_conf.bwg_dim, 22)
+        else:
+            grad_weight_ = grad_weight
 
         # Apply weaken gradient if weight gradient boost is applied
         # if bfp_conf.bwg_boost != 1.0:
@@ -249,7 +231,7 @@ class BFPConv2dFunction(torch.autograd.Function):
         if bias is not None and ctx.needs_input_grad[2]:
             grad_bias = grad_output_.sum(dim=(0,2,3)).squeeze(0)
         
-        return grad_input, grad_weight, grad_bias, None, None, None, None, None
+        return grad_input_, grad_weight_, grad_bias, None, None, None, None, None
 
 # Blockfloat Convolution
 class BFPConv2d(torch.nn.Module):
