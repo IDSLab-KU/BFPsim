e5240f889fed4697fd2a8646e063d7d070934517
r3coder <coder@dgist.ac.kr>
2022-04-20 05:58:12 +0000

    fix: updated stupid zse getting code i'm dumb

--- bfp/internal.py
+++ bfp/internal.py
@@ -291,89 +291,154 @@ def gradient_linear_weight_2d(grad_output, input, weight_shape):
 
 
 
-
-@cuda.jit
 # TODO : make another function to just grouping tensor...?
-def get_zse_internal(v, res, bs, gs, group_mantissa):
+@cuda.jit
+def get_precision_internal(v, res, bs, gd):
     
-    idx = cuda.threadIdx.x + cuda.blockDim.x  * cuda.blockIdx.x     
-    idx0o = (idx // (bs[3] * bs[2] * bs[1])) * gs[0]
-    idx1o = (idx // (bs[3] * bs[2])) % bs[1] * gs[1]
-    idx2o = (idx // bs[3]) % bs[2] * gs[2]
-    idx3o = idx % bs[3] * gs[3]
+    idx = cuda.threadIdx.x + cuda.blockDim.x  * cuda.blockIdx.x    
+    
+    i0g =  idx // (bs[3]*bs[2]*bs[1])
+    i1g = (idx // (bs[3]*bs[2])) % bs[1]
+    i2g = (idx //  bs[3]) % bs[2]
+    i3g =  idx % bs[3]
 
+    i0s, i1s, i2s, i3s = i0g*gd[0], i1g*gd[1], i2g*gd[2], i3g*gd[3]
+    i0e = i0s+gd[0] if i0s+gd[0] < v.shape[0] else v.shape[0]
+    i1e = i1s+gd[1] if i1s+gd[1] < v.shape[1] else v.shape[1]
+    i2e = i2s+gd[2] if i2s+gd[2] < v.shape[2] else v.shape[2]
+    i3e = i3s+gd[3] if i3s+gd[3] < v.shape[3] else v.shape[3]
     # Find the max exponent from each group
     M = 0
-    for idx0 in range(idx0o, idx0o + gs[0]):
-        if idx0 >= v.shape[0]:
-            break
-        for idx1 in range(idx1o, idx1o + gs[1]):
-            if idx1 >= v.shape[1]:
-                break
-            for idx2 in range(idx2o, idx2o + gs[2]):
-                if idx2 >= v.shape[2]:
-                    break
-                for idx3 in range(idx3o, idx3o + gs[3]):
-                    if idx3 >= v.shape[3]:
-                        break
+    for idx0 in range(i0s, i0e):
+        for idx1 in range(i1s, i1e):
+            for idx2 in range(i2s, i2e):
+                for idx3 in range(i3s, i3e):
+                    res[idx0,idx1,idx2,idx3] = (v[idx0,idx1,idx2,idx3] & 0x7f800000 ) >> 23
+
+    cuda.syncthreads()
+
+
+# get_zse : Get ZSE of a 4d tensor
+def get_precision(inp, group_mantissa, group_dim):
+    print("get_precision executed:")
+    # Make true to ZSE analyze, temporal disabled
+    # Set pointer of tensor as int, easier to manipulate
+    inp_ = inp.view(torch.int32)
+    ins = np.array(inp.size())
+    # Array to store result
+    
+    if len(ins) == 4: # If the tensor size is 4d
+        # Choose Ideal thread size
+
+        # Set blockspergrid and call internal function
+        bs = ((ins[0]-1)//group_dim[0]+1, (ins[1]-1)//group_dim[1]+1, (ins[2]-1)//group_dim[2]+1, (ins[3]-1)//group_dim[3]+1)
+        inpsize = (ins[0], ins[1], ins[2], ins[3])
+        
+        res = np.zeros(inp.size())
+
+        threads = np.product(bs)
+        threads = threads + (32 - threads % 32)
+        if threads > 512: # Adjusted here!
+            threads = 512
+        threads = int(threads)
+        blockspergrid = (np.product(bs) +  (threads - 1)) // threads
+        
+        # args = (bs[0], bs[1], bs[2], bs[3], group_dim[0], group_dim[1], group_dim[2], group_dim[3], group_mantissa)
+
+        # print(ins, group_mantissa, group_dim, bs, res.shape, threads, blockspergrid, end="  ")
+        get_precision_internal[blockspergrid, threads](inp_, res, bs, group_dim)
+
+    else: # Tensor dimension is not supported
+        print("get_zse ERROR: Tensor dimension not supported %s"%(str(inp.size())))
+    s = "Data:\n"
+    for i0 in range(inp.shape[0]):
+        for i1 in range(inp.shape[1]):
+            s += " ======== %d, %d ========\n"%(i0, i1)
+            for i2 in range(inp.shape[2]):
+                for i3 in range(inp.shape[3]):
+                    s += "%4.2f %3d\t"%(inp[i0,i1,i2,i3], res[i0,i1,i2,i3])
+            s += "\n"
+    print(s)
+
+    return a
+
+
+# TODO : make another function to just grouping tensor...?
+@cuda.jit
+def get_zse_internal(v, res, bs, gd, gm):
+
+    idx = cuda.threadIdx.x + cuda.blockDim.x  * cuda.blockIdx.x    
+    
+    i0g =  idx // (bs[3]*bs[2]*bs[1])
+    i1g = (idx // (bs[3]*bs[2])) % bs[1]
+    i2g = (idx //  bs[3]) % bs[2]
+    i3g =  idx % bs[3]
+
+    i0s, i1s, i2s, i3s = i0g*gd[0], i1g*gd[1], i2g*gd[2], i3g*gd[3]
+    i0e = i0s+gd[0] if i0s+gd[0] < v.shape[0] else v.shape[0]
+    i1e = i1s+gd[1] if i1s+gd[1] < v.shape[1] else v.shape[1]
+    i2e = i2s+gd[2] if i2s+gd[2] < v.shape[2] else v.shape[2]
+    i3e = i3s+gd[3] if i3s+gd[3] < v.shape[3] else v.shape[3]
+    # Find the max exponent from each group
+    M = 0
+    for idx0 in range(i0s, i0e):
+        for idx1 in range(i1s, i1e):
+            for idx2 in range(i2s, i2e):
+                for idx3 in range(i3s, i3e):
                     e = (v[idx0,idx1,idx2,idx3] & 0x7f800000 ) >> 23
                     if M < e:
                         M = e
     if M == 0:
         return
-    
-    idx0g = idx // (bs[3] * bs[2] * bs[1])
-    idx1g = (idx // (bs[3] * bs[2])) % bs[1]
-    idx2g = (idx // bs[3]) % bs[2]
-    idx3g = idx % bs[3]
 
     rs = 0
     # Remove each mantissa to desired values
-    for idx0 in range(idx0o, idx0o + gs[0]):
-        if idx0 >= v.shape[0]:
-            break
-        for idx1 in range(idx1o, idx1o + gs[1]):
-            if idx1 >= v.shape[1]:
-                break
-            for idx2 in range(idx2o, idx2o + gs[2]):
-                if idx2 >= v.shape[2]:
-                    break
-                for idx3 in range(idx3o, idx3o + gs[3]):
-                    if idx3 >= v.shape[3]:
-                        break
+    for idx0 in range(i0s, i0e):
+        for idx1 in range(i1s, i1e):
+            for idx2 in range(i2s, i2e):
+                for idx3 in range(i3s, i3e):
                     e = (v[idx0,idx1,idx2,idx3] & 0x7f800000 ) >> 23
-                    if M - e + 1 > group_mantissa:
+                    if M - e + 1 > gm:
                         rs += 1
-    res[idx0g, idx1g, idx2g, idx3g] = rs
+    res[i0g, i1g, i2g, i3g] = rs
     cuda.syncthreads()
+
 
 # get_zse : Get ZSE of a 4d tensor
 def get_zse(inp, group_mantissa, group_dim):
-    # Make true to ZSE analyze, temporal disabled
-    # if FLAGS.ZSE:
-    #     analyzeObject.AddData(inp.clone().detach(), group_mantissa, group_dim, type)
 
+    # Make true to ZSE analyze, temporal disabled
     # Set pointer of tensor as int, easier to manipulate
     inp_ = inp.view(torch.int32)
     ins = np.array(inp.size())
     # Array to store result
-    res = np.zeros((ins[0]//group_dim[0], ins[1]//group_dim[1], ins[2]//group_dim[2], ins[3]//group_dim[3]))
+    
     if len(ins) == 4: # If the tensor size is 4d
         # Choose Ideal thread size
-        threads = (ins[0]*ins[1]*ins[2]*ins[3]) // (group_dim[0]*group_dim[1]*group_dim[2]*group_dim[3])
+
+        # Set blockspergrid and call internal function
+        bs = ((ins[0]-1)//group_dim[0]+1, (ins[1]-1)//group_dim[1]+1, (ins[2]-1)//group_dim[2]+1, (ins[3]-1)//group_dim[3]+1)
+        inpsize = (ins[0], ins[1], ins[2], ins[3])
+        
+        res = np.zeros(bs)
+
+        threads = np.product(bs)
         threads = threads + (32 - threads % 32)
         if threads > 512: # Adjusted here!
             threads = 512
         threads = int(threads)
+        blockspergrid = (np.product(bs) +  (threads - 1)) // threads
+        
+        # args = (bs[0], bs[1], bs[2], bs[3], group_dim[0], group_dim[1], group_dim[2], group_dim[3], group_mantissa)
 
-        # Set blockspergrid and call internal function
-        bs = ((ins[0]-1)//group_dim[0]+1, (ins[1]-1)//group_dim[1]+1, (ins[2]-1)//group_dim[2]+1, (ins[3]-1)//group_dim[3]+1)
-        blockspergrid = (ins[0]*ins[1]*ins[2]*ins[3] +  (threads - 1)) // threads
-        inpsize = (ins[0], ins[1], ins[2], ins[3])
+        # print(ins, group_mantissa, group_dim, bs, res.shape, threads, blockspergrid, end="  ")
         get_zse_internal[blockspergrid, threads](inp_, res, bs, group_dim, group_mantissa)
+
     else: # Tensor dimension is not supported
-        inpsize = (ins[0], ins[1], ins[2], ins[3])
-        print("make_groups_tensor ERROR: Tensor dimension not supported %s"%(str(inpsize)))
+        print("get_zse ERROR: Tensor dimension not supported %s"%(str(inp.size())))
+
     # print(np.sum(res) / np.prod(ins))
 
-    return np.sum(res) / np.prod(ins) 
+    a = np.sum(res) / np.prod(ins)
+    # print("%14.5f  %14d  %14d"%(a, np.sum(res), np.prod(ins)))
+    return a
